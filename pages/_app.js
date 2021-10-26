7dc821d710522a8b5d2df75be804f039f1992658
nabenitez <nabenitez@uc.cl>
2021-10-26 10:34:22 -0300

    chore: update deps

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ function MyApp(props) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>Witby</title>
+        <title>Taller</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
