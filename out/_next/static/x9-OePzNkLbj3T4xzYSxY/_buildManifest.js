3d88814b98fe62cdd261ae3c27c8d182d39f378d
nabenitez <nabenitez@uc.cl>
2021-10-27 13:50:58 -0300

    add: out folder

---
+++ out/_next/static/x9-OePzNkLbj3T4xzYSxY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/457-0c93bb28e7c883662277.js","static/chunks/pages/index-bf0c45b88392865a3ac2.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
