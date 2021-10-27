c2ff7a23a0c2540324a548fdfcb46eddf9fae84b
nabenitez <nabenitez@uc.cl>
2021-10-26 22:51:47 -0300

    feat(map): add real time map with markers and polylines

---
+++ components/Map.js
@@ -0,0 +1,55 @@
+import 'leaflet/dist/leaflet.css'
+import React from 'react'
+import { icon } from 'leaflet'
+import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet'
+import socket from '../services/ws.service'
+import randomColor from 'randomcolor'
+
+const ICON = icon({
+  iconUrl: '/marker.png',
+  iconSize: [10, 12],
+})
+
+const Map = () => {
+  const [trucksPositions, setTrucksPositions] = React.useState({})
+
+  React.useEffect(() => {
+    socket.on('POSITION', (message) => {
+      const { code, position } = message
+      if (trucksPositions[code]) {
+        trucksPositions[code] = {
+          ...trucksPositions[code],
+          latLng: [...trucksPositions[code]?.latLng, position],
+        }
+      } else {
+        trucksPositions[code] = { color: randomColor(), latLng: [position] }
+      }
+
+      setTrucksPositions({
+        ...trucksPositions,
+      })
+    })
+  }, [])
+
+  return Object.values(trucksPositions)[0] ? (
+    <MapContainer
+      center={Object.values(trucksPositions)[0].latLng[0]}
+      zoom={11}
+      scrollWheelZoom={false}
+      style={{ height: '50vh', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {Object.values(trucksPositions).map((pos, index) => (
+        <div key={index}>
+          <Polyline positions={pos.latLng} pathOptions={{ color: pos.color }} />
+          <Marker position={pos.latLng.slice(-1)[0]} icon={ICON} />
+        </div>
+      ))}
+    </MapContainer>
+  ) : null
+}
+
+export default Map
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,45 @@
 import * as React from 'react'
 import Container from '@mui/material/Container'
-import Typography from '@mui/material/Typography'
-import Link from 'next/link'
 import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import dynamic from 'next/dynamic'
+import { connect } from 'socket.io-client'
 
 export default function Index() {
+  React.useEffect(() => {
+    // connect to socket server
+    const socket = connect(process.env.NEXT_PUBLIC_WS_BASE_URL, {
+      path: '/trucks',
+    })
+
+    // log socket connection
+
+    // update chat on new message dispatched
+    socket.emit('TRUCKS')
+
+    socket.on('TRUCKS', (message) => {
+      console.log('trucks', message)
+    })
+
+    // socket disconnet onUnmount if exists
+    if (socket) return () => socket.disconnect()
+  }, [])
+  const MapNoSSR = dynamic(() => import('../components/Map'), { ssr: false })
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 'auto', mx: 'auto' }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Example app
-        </Typography>
-        <Link href="/signin">Iniciar sesión</Link>
-      </Box>
+    <Container sx={{ mt: 8 }} maxWidth="lg">
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={7}>
+          <MapNoSSR />
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <Box sx={{ bgcolor: 'primary.main', height: '50vh', width: '100%' }}>
+            a
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Box sx={{ bgcolor: 'red', height: '30vh', width: '100%' }}></Box>
+        </Grid>
+      </Grid>
     </Container>
   )
 }
